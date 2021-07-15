c9bdf86c3cb9f9df3f5f363a64d4860b04533f62
chiragfalor <67641147+chiragfalor@users.noreply.github.com>
2021-07-15 00:00:25 -0400

    Made changes to deal with ellipses

--- quadruply_lensed_quasars.py
+++ quadruply_lensed_quasars.py
@@ -22,7 +22,14 @@ class Conic():
     '''
     return np.sqrt((c1[0] - c2[0])**2 + (c1[1] - c2[1])**2)
 
-
+  def coordinate_difference(self, c1, c2):
+    '''
+    input : 2 tuples (coordinates)
+    output : 1 tuple (coordinate)
+    Takes in 2 coordinates c1 and c2 and returns c1 - c2
+    '''
+    newc = (c1[0]- c2[0], c1[1] - c2[1])
+    return newc
 
 class Circle(Conic):
     def __init__(self, c1, c2, c3):
@@ -67,7 +74,7 @@ class Circle(Conic):
         plt.show()
 
 class AlignedEllipse(Conic):
-    def __init__(self, c1, c2, c3, c4):
+    def __init__(self, c1, c2, c3, c4, iscentered=False):
         '''
         initialize a rectangular hyperbola in cartesian coordinates where c1, c2, c3, c4 are tuples of coordinates in the form of (x_i, y_i)
         '''
@@ -75,14 +82,73 @@ class AlignedEllipse(Conic):
         self.c2 = c2
         self.c3 = c3
         self.c4 = c4
-        equation_matrix = np.array([self.equation_maker_hyperbola(c1), self.equation_maker_hyperbola(c2), self.equation_maker_hyperbola(c3), self.equation_maker_hyperbola(c4)])
+        equation_matrix = np.array([self.equation_maker_ellipse(c1), self.equation_maker_ellipse(c2), self.equation_maker_ellipse(c3), self.equation_maker_ellipse(c4)])
         equation_solutions = np.array([1,1,1,1])
         '''
-        Assumes that equation of rectangular hyperbola is of the form A(x^2 - y^2) + Bxy + Cx + Dy = 1 and solves the 4 equations got by substituting the 4 coordinates
+        Assumes that equation of ellipse is of the form Ax^2 + By^2 + Cx + Dy = 1 and solves the 4 equations got by substituting the 4 coordinates
         '''
         self.coefficients = np.linalg.solve(equation_matrix, equation_solutions)
-        self.center = self.findCenter()
-        self.equation = str(self.coefficients[0]) + "(x^2 - y^2) + " + str(self.coefficients[1]) + "xy + " + str(self.coefficients[2]) + "x + " + str(self.coefficients[3]) + "y = 1"
+        self.center = self.findCenter_ellipse()
+        self.equation = str(self.coefficients[0]) + "x^2 + " + str(self.coefficients[1]) + "y^2 + " + str(self.coefficients[2]) + "x + " + str(self.coefficients[3]) + "y = 1"
+        self.centered_ellipse_coordinates = [self.coordinate_difference(c1, self.center),self.coordinate_difference(c2, self.center),self.coordinate_difference(c3, self.center),self.coordinate_difference(c4, self.center)]
+        if iscentered:
+          if (self.center[0])**2 + (self.center[1])**2>0.000001:
+            raise(Exception("Ellipse is not centered"))
+          if self.coefficients[0]<0 or self.coefficients[1]<0:
+            raise(Exception("This is not ellipse but hyperbola or imaginary figure"))
+          self.a = 1/np.sqrt(self.coefficients[0])
+          self.b = 1/np.sqrt(self.coefficients[1])
+          self.parametric_angles = [self.coordinate_to_parametric_angle(c1), self.coordinate_to_parametric_angle(c2), self.coordinate_to_parametric_angle(c3), self.coordinate_to_parametric_angle(c4)]
+        else:
+          centered_ellipse = AlignedEllipse(self.centered_ellipse_coordinates[0], self.centered_ellipse_coordinates[1],self.centered_ellipse_coordinates[2],self.centered_ellipse_coordinates[3], True)
+          self.a = centered_ellipse.a
+          self.b = centered_ellipse.b
+          self.parametric_angles = centered_ellipse.parametric_angles
+        self.normalized_coordinates = list(map(self.coordinate_normalizer, self.centered_ellipse_coordinates))
+          
+    def coordinate_normalizer(self,c1):
+      nc = (c1[0]/self.a, c1[1]/self.b)
+      return nc
+
+    def coordinate_to_parametric_angle(self, c1):
+      nc = (c1[0]/self.a, c1[1]/self.b)
+      return np.arctan2(nc[1], nc[0])
+      
+
+    def findCenter_ellipse(self):
+        '''
+        returns the center of the current hyperbola by partial differentiation of the equation wrt x and y separately and solving the linear equation
+        '''
+        A, B, C, D = self.coefficients
+        x_c = -C/(2*A)
+        y_c = -D/(2*B)
+        return (x_c, y_c)
+
+    def equation_maker_ellipse(self, c1):
+        '''
+        input: tuple (Coordinate)
+        output: from tuple(x,y), it returns (x**2-y**2, x*y, x, y)
+        '''
+        x = c1[0]
+        y = c1[1]
+        return (x**2, y**2, x, y)
+
+    def plot(self):
+        x = np.linspace(-3, 3, 400)
+        y = np.linspace(-3, 3, 400)
+        x, y = np.meshgrid(x, y)
+        a, b, c, d, e, f = self.coefficients[0], self.coefficients[1], -self.coefficients[0], self.coefficients[2], self.coefficients[3], -1
+        def axes():
+            plt.axhline(0, alpha=.1)
+            plt.axvline(0, alpha=.1)
+        axes()
+        plt.contour(x, y,(a*x**2 + b*x*y + c*y**2 + d*x + e*y + f), [0], colors='k')
+        plt.plot(self.c1[0], self.c1[1], 'r+')
+        plt.plot(self.c2[0], self.c2[1], 'r+')
+        plt.plot(self.c3[0], self.c3[1], 'r+')
+        plt.plot(self.c4[0], self.c4[1], 'r+')
+        plt.show()
+
 
 
 class RectangularHyperbola(Conic):
@@ -358,8 +424,8 @@ class Quasar(Conic):
   def calculate_astroidal_angle(self):
     x_sum = np.sum(self.quasar_norm_array[:,0])
     y_sum = np.sum(self.quasar_norm_array[:,1])
-    alpha = np.arctan2(y_sum,x_sum)
-    alpha_new = np.arctan(np.tan(self.configuration_angles[1])*np.tan(self.configuration_angles[2])*np.tan((self.configuration_angles[1] + self.configuration_angles[2])/2))
+    alpha = np.arctan2(np.cbrt(y_sum),np.cbrt(x_sum))
+    alpha_new = np.arctan(np.cbrt(np.tan(self.configuration_angles[1])*np.tan(self.configuration_angles[2])*np.tan((self.configuration_angles[1] + self.configuration_angles[2])/2)))
     if self.configuration_angles[1]>np.pi/2:
       alpha_new += np.pi
     elif self.configuration_angles[1]<-np.pi/2:
@@ -431,8 +497,51 @@ class Quasar(Conic):
   def __repr__(self):
     return str(self.configuration_angles)
     
+class NonCircularQuasar(Quasar):
+  def __init__(self, coordinate_list):
+    self.c1, self.c2, self.c3, self.c4 = coordinate_list
+    c1, c2, c3, c4 = self.c1, self.c2, self.c3, self.c4
+    self.quasar_circle = Circle(c1, c2, c3)
+    self.quasar_hyperbola = RectangularHyperbola(c1, c2, c3, c4)
+    self.quasar_hyperbola_center = self.quasar_hyperbola.center
+    self.quasar_norm_array = np.array([self.c1, self.c2, self.c3, self.c4])
+    self.configuration_angles = np.arctan2(self.quasar_norm_array[:, 1], self.quasar_norm_array[:, 0])
+    self.configurationInvariant = self.Calculate_configuration_invariant(self.configuration_angles)
+    self.sort_configuration()
 
+    self.psi = self.psi_calculator()
 
+    self.quasar_rotator(-self.psi)
+    self.causticity = self.calculate_causticity()
+    self.diff_WW_KK = self.WW_invariant_calculator(self.configuration_angles)
+
+    self.new_causticity = self.calculate_new_causticity()
+    if (self.new_causticity-self.causticity)**2 > 0.00001:
+      print(self.new_causticity, self.causticity)
+    self.astroidal_angle = self.calculate_astroidal_angle()
+
+def AlignedEllipse_test():
+  P1 = (2,1)
+  P2 = (-1, 0)
+  P3 = (1,1)
+  P4 = (1, -1)
+  S2P1 = (-3.467548592968,4.2937233604296)
+  S2P2 = (-3.0360531925048,3.992103466708)
+  S2P3 = (-4.0176241335422,1.6496079065768)
+  S2P4 = (-5.6870476943574,3.5865672944457)
+  el = AlignedEllipse(S2P1, S2P2, S2P3, S2P4)
+  print(el.parametric_angles)
+  print(el.a, el.b)
+  print(el.normalized_coordinates)
+  NCQ = NonCircularQuasar(el.normalized_coordinates)
+  print("NCQ configuration invariant",NCQ.configurationInvariant)
+  print("NCQ delta theta_23", NCQ.diff_WW_KK)
+  print(NCQ.quasar_norm_array)
+
+AlignedEllipse_test()
+
+def Saturn_Flash_test():
+    pass
 
 
 
@@ -1188,7 +1297,7 @@ def Quasar_random_shear_plot_try2():
 #Quasar_list = Quasar_random_ca_plot_four_sided()
 
 #Quasar_list = Quasar_random_shear_plot()
-
+'''
 with open("new_plot_ca.txt", "rb") as fp:   # Unpickling
   new_Quasar_list = pickle.load(fp)
 new_Quasar_list[2][0].plot()
@@ -1198,6 +1307,7 @@ ax.set_xlim(-1.05, 1)
 ax.set_ylim(-1.05,1)
 plt.savefig('Labeling_conv.pdf')
 plt.show()
+'''
 #plot_quasars_shear_try2(new_Quasar_list)
 
 '''
