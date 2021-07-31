31ada1fb04123e1557ff901273cf9014deb0286e
chiragfalor <67641147+chiragfalor@users.noreply.github.com>
2021-07-30 20:14:23 -0400

    Added aligned ellipse class

--- quadruply_lensed_quasars.py
+++ quadruply_lensed_quasars.py
@@ -70,7 +70,81 @@ class Circle(Conic):
         plt.plot(self.c3[0], self.c3[1], 'r+')
         plt.show()
 
+class AlignedEllipse(Conic):
+    def __init__(self, c1, c2, c3, c4, iscentered=False):
+        '''
+        initialize a rectangular hyperbola in cartesian coordinates where c1, c2, c3, c4 are tuples of coordinates in the form of (x_i, y_i)
+        '''
+        self.c1 = c1
+        self.c2 = c2
+        self.c3 = c3
+        self.c4 = c4
+        equation_matrix = np.array([self.equation_maker_ellipse(c1), self.equation_maker_ellipse(c2), self.equation_maker_ellipse(c3), self.equation_maker_ellipse(c4)])
+        equation_solutions = np.array([1,1,1,1])
+        '''
+        Assumes that equation of ellipse is of the form Ax^2 + By^2 + Cx + Dy = 1 and solves the 4 equations got by substituting the 4 coordinates
+        '''
+        self.coefficients = np.linalg.solve(equation_matrix, equation_solutions)
+        self.center = self.findCenter_ellipse()
+        self.equation = str(self.coefficients[0]) + "x^2 + " + str(self.coefficients[1]) + "y^2 + " + str(self.coefficients[2]) + "x + " + str(self.coefficients[3]) + "y = 1"
+        self.centered_ellipse_coordinates = [self.coordinate_difference(c1, self.center),self.coordinate_difference(c2, self.center),self.coordinate_difference(c3, self.center),self.coordinate_difference(c4, self.center)]
+        if iscentered:
+          if (self.center[0])**2 + (self.center[1])**2>0.000001:
+            raise(Exception("Ellipse is not centered"))
+          if self.coefficients[0]<0 or self.coefficients[1]<0:
+            raise(Exception("This is not ellipse but hyperbola or imaginary figure"))
+          self.a = 1/np.sqrt(self.coefficients[0])
+          self.b = 1/np.sqrt(self.coefficients[1])
+          self.parametric_angles = [self.coordinate_to_parametric_angle(c1), self.coordinate_to_parametric_angle(c2), self.coordinate_to_parametric_angle(c3), self.coordinate_to_parametric_angle(c4)]
+        else:
+          centered_ellipse = AlignedEllipse(self.centered_ellipse_coordinates[0], self.centered_ellipse_coordinates[1],self.centered_ellipse_coordinates[2],self.centered_ellipse_coordinates[3], True)
+          self.a = centered_ellipse.a
+          self.b = centered_ellipse.b
+          self.parametric_angles = centered_ellipse.parametric_angles
+        self.normalized_coordinates = list(map(self.coordinate_normalizer, self.centered_ellipse_coordinates))
+          
+    def coordinate_normalizer(self,c1):
+      nc = (c1[0]/self.a, c1[1]/self.b)
+      return nc
 
+    def coordinate_to_parametric_angle(self, c1):
+      nc = (c1[0]/self.a, c1[1]/self.b)
+      return np.arctan2(nc[1], nc[0])
+      
+
+    def findCenter_ellipse(self):
+        '''
+        returns the center of the current hyperbola by partial differentiation of the equation wrt x and y separately and solving the linear equation
+        '''
+        A, B, C, D = self.coefficients
+        x_c = -C/(2*A)
+        y_c = -D/(2*B)
+        return (x_c, y_c)
+
+    def equation_maker_ellipse(self, c1):
+        '''
+        input: tuple (Coordinate)
+        output: from tuple(x,y), it returns (x**2-y**2, x*y, x, y)
+        '''
+        x = c1[0]
+        y = c1[1]
+        return (x**2, y**2, x, y)
+
+    def plot(self):
+        x = np.linspace(-3, 3, 400)
+        y = np.linspace(-3, 3, 400)
+        x, y = np.meshgrid(x, y)
+        a, b, c, d, e, f = self.coefficients[0], self.coefficients[1], -self.coefficients[0], self.coefficients[2], self.coefficients[3], -1
+        def axes():
+            plt.axhline(0, alpha=.1)
+            plt.axvline(0, alpha=.1)
+        axes()
+        plt.contour(x, y,(a*x**2 + b*x*y + c*y**2 + d*x + e*y + f), [0], colors='k')
+        plt.plot(self.c1[0], self.c1[1], 'r+')
+        plt.plot(self.c2[0], self.c2[1], 'r+')
+        plt.plot(self.c3[0], self.c3[1], 'r+')
+        plt.plot(self.c4[0], self.c4[1], 'r+')
+        plt.show()
 
 class RectangularHyperbola(Conic):
     def __init__(self, c1, c2, c3, c4):
